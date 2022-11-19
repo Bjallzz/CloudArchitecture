4d0199178e04b0d64f9ad0df7ca36dc69737c991
macieksputowski <maciejsputowski@onet.pl>
2022-11-19 10:34:18 +0100

    Command controller

---
+++ lab2/source/MyWebApp/Controllers/CommandController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        // GET: api/<CommandController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
