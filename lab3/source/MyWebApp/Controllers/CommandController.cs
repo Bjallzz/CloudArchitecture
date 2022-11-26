43b327db78c590fd3da702e7d668d2c4740979b7
macieksputowski <maciejsputowski@onet.pl>
2022-11-26 10:20:09 +0100

    lab3 Initial commit

---
+++ lab3/source/MyWebApp/Controllers/CommandController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        // GET: api/<CommandController>
+        [HttpGet]
+        public IActionResult Get([FromQuery] string? cmd, [FromQuery] string? str)
+        {
+            if(!string.IsNullOrEmpty(cmd))
+            {
+                switch (cmd) 
+                {
+                    case "time":
+                        return Ok(DateTime.Now.ToString("HH:mm:ss"));
+                    case "rev":
+                        if(string.IsNullOrEmpty(str))
+                        {
+                            return BadRequest("Command rev was supplied but str parameter was null or empty");
+                        }
+                        return Ok(string.Concat(str.Reverse()));
+                }
+            }
+
+            return Ok("Hello World");
+        }
+    }
+}
