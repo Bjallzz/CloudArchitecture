6e19383963aca463efdad1101d0f6a2b620b68db
macieksputowski <maciejsputowski@onet.pl>
2022-11-26 10:28:56 +0100

    StringController

---
+++ lab3/source/MyWebApp/Controllers/StringController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Models;
+
+namespace MyWebApp.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class StringController : ControllerBase
+    {
+        // GET: api/<CommandController>
+        [HttpGet]
+        public IActionResult Get([FromQuery] string? str)
+        {
+            if(str is null)
+            {
+                return BadRequest("No string was supplied");
+            }
+
+            if(str == string.Empty)
+            {
+                return Ok(new StringBreakdown());
+            }
+
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ lab3/source/MyWebApp/Models/StringBreakdown.cs
@@ -0,0 +1,10 @@
+﻿namespace MyWebApp.Models
+{
+    public class StringBreakdown
+    {
+        public int Lowercase { get; set; } = 0;
+        public int Uppercase { get; set; } = 0;
+        public int Digits { get; set; } = 0;
+        public int Special { get; set; } = 0;
+    }
+}
